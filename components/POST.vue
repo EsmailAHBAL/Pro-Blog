ab665e849b4b19944f73093a52e5f7841360ab60
Esmail Ahbal <100035479+EsmailAHBAL@users.noreply.github.com>
2022-10-14 15:05:41 -0400

    s-f

--- components/POST.vue
+++ components/POST.vue
@@ -4,7 +4,7 @@
 
       <div class="block mt-4">
         <div class="block">
-          <span class="tag is-dark">{{comment['0']['author']}}</span>
+          <span class="tag is-dark">{{comment['0'].author}}</span>
         </div>
 
       </div>
@@ -21,10 +21,10 @@
       </div>
       <div class="container ">
         <el-row :gutter="20" class="pb-3">
-        <el-col :span="12"><span class="tag is-dark">{{comment['0']['author']}}</span></el-col>
-        <el-col :span="12"><span class="tag is-danger">{{comment['0']['source']['name']}}</span></el-col>
-        <el-col :span="12"><span class="tag is-dark mt-3">{{comment['0']['publishedAt']}}</span></el-col>
-        <el-col :span="12"><span class="tag is-danger mt-3">{{comment['0']['url']}}</span></el-col>
+        <el-col :span="12"><span class="tag is-dark">{{comment['0'].author}}</span></el-col>
+        <el-col :span="12"><span class="tag is-danger">{{comment['0'].source.name}}</span></el-col>
+        <el-col :span="12"><span class="tag is-dark mt-3">{{comment['0'].publishedAt}}</span></el-col>
+        <el-col :span="12"><span class="tag is-danger mt-3">{{comment['0'].url}}</span></el-col>
 
       </el-row>
       <div class="block mt-3">
